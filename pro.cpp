560c70b6d3451a17b405bddbea813deba332313f
AbhipsaMishra7 <42178479+AbhipsaMishra7@users.noreply.github.com>
2018-08-17 11:05:50 +0530

    Add files via upload

---
+++ pro.cpp
@@ -0,0 +1,20 @@
+#include<iostream> //Defines Library
+using namespace std ; //acccepts the standard definition
+int main () { //taking Integer to be the main function
+int  x,y ; // Declaring integer variables
+x = 256 ; y = 252 ; //assigning values to variables
+int sum = x + y ; //addition
+int difference = x - y ; //subtraction
+int product = x * y ; //multiplication
+int quotient = x / y ; //division
+int remainder = x % y ; //modulus function 
+
+cout << sum << endl ; //print sum
+cout << difference << endl ; //print difference
+cout << product << endl ; //print product
+cout << quotient << endl ; //print quoteint
+cout << remainder << endl ; //print remainder
+
+return 7 ; //returning an integer value
+}
+
